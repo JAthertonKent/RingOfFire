0059eebaa8354fe3fd4b983ef15fc53b7b44ebb7
nphollon <nhollon@thoughtworks.com>
2013-07-16 09:16:09 -0700

    Extracted some methods

--- map.js
+++ map.js
@@ -1,30 +1,36 @@
-var map;
-function initialize() {
+function renderMap() {
+  var map = getBlankMap();
+  populate(map);
+}
+
+function getBlankMap() {
   var usa = {'latitude': 40, 'logitude': -99};
   var mapOptions = {
     zoom: 4,
     center: new google.maps.LatLng(usa['latitude'], usa['logitude']),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-  createMarkers(map);  
+  return new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-function createMarkers(map) {
+function populate(map) {
   var options = 'https://soda.demo.socrata.com/resource/earthquakes.json';
 
   $.get(options, function(response) {
     for (var i = 0; i < response.length; i++) {
-      var location = response[i]['location'] 
-    
-      new google.maps.Marker({
-        position: new google.maps.LatLng(location['latitude'], location['longitude']),
-        map: map
-      });
+      createMarker(map, response[i]);
     }
   });
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function createMarker(map, earthquake) {
+  var location = earthquake['location'];
+  return new google.maps.Marker({
+    position: new google.maps.LatLng(location['latitude'], location['longitude']),
+    map: map
+  });
+}
+
+$(document).ready(function() {
+  google.maps.event.addDomListener(window, 'load', renderMap);
+});
